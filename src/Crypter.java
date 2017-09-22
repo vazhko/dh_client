51827a92faeca6736a71bbba2d86e656f89ef950
Vladislav.Vazhko <Vladislav.Vazhko@dp-atlas.com.ua>
2017-09-22 16:27:04 +0300

    Попытка синхронизации ключей

--- src/Crypter.java
+++ src/Crypter.java
@@ -1,23 +1,17 @@
 //package client;
+import javax.xml.bind.DatatypeConverter;
 
 import java.io.IOException;
-
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-
 import java.nio.file.Paths;
-
 import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
-
 import java.security.MessageDigest;
-
 import java.security.PrivateKey;
-
 import java.security.PublicKey;
-
 import java.security.spec.X509EncodedKeySpec;
-
 import java.util.Arrays;
 
 import javax.crypto.Cipher;
@@ -74,6 +68,10 @@ public class Crypter {
     public PublicKey getClientPublicKey() {
         return clientPublicKey;
     }
+    
+    public PublicKey getServerPublicKey() {
+        return serverPublicKey;
+    }
 
     public synchronized int generateClientKeys() {
         int crypterError = 0;
@@ -83,6 +81,10 @@ public class Crypter {
             KeyPair clientKeyPair = keyPairGenerator.genKeyPair();
             clientPrivateKey = clientKeyPair.getPrivate();
             clientPublicKey = clientKeyPair.getPublic();
+            //byte[] bkeyCliPub = Arrays.copyOf(sha256.digest(secretDevice), AES_KEY_SIZE / Byte.SIZE);
+            String hex = DatatypeConverter.printHexBinary(clientPublicKey.getEncoded());
+            System.out.println(hex);
+            
 
         } catch (Exception e) {
             crypterError = 2;
@@ -102,9 +104,17 @@ public class Crypter {
                    keyAgreement.doPhase(serverPublicKey, true);
 
                    byte[] secretDevice = keyAgreement.generateSecret();
+                   
+                   String hex = DatatypeConverter.printHexBinary(secretDevice);
+                   System.out.println("secretDevice=");
+                   System.out.println(hex);
+                   System.out.println("");
 
+                   
                    byte[] bkeyDevice = Arrays.copyOf(sha256.digest(secretDevice), AES_KEY_SIZE / Byte.SIZE);
-
+                   hex = DatatypeConverter.printHexBinary(bkeyDevice);
+                   System.out.println(hex);
+                   
                    clientAESKey = new SecretKeySpec(bkeyDevice, "AES");
                }
 
@@ -117,6 +127,11 @@ public class Crypter {
        }
     public synchronized int setServerPublicKey(byte[] data) {
            int crypterError = 0;
+           
+           String hex = DatatypeConverter.printHexBinary(data);
+           System.out.println("input=");
+           System.out.println(hex);
+           System.out.println("");
 
            try {
                this.serverPublicKey = keyFactory.generatePublic(new X509EncodedKeySpec(data));
@@ -132,13 +147,17 @@ public class Crypter {
 
            byte[] cryptedData = null;
 
-           try {
+           try {        	   
                cipherAES.init(Cipher.ENCRYPT_MODE, clientAESKey);
+               /*
                byte[] cryptedUpdate = cipherAES.update(data, where, length);
                byte[] cryptedFinal = cipherAES.doFinal();
                cryptedData = new byte[cryptedUpdate.length + cryptedFinal.length];
                System.arraycopy(cryptedUpdate, 0, cryptedData, 0, cryptedUpdate.length);
                System.arraycopy(cryptedFinal, 0, cryptedData, cryptedUpdate.length, cryptedFinal.length);
+               */
+               String plainText = "Ura!!!!!";
+               return cipherAES.doFinal(plainText.getBytes());
            } catch (Exception e) {
                e.printStackTrace();
            }
@@ -151,6 +170,7 @@ public class Crypter {
 
           try {
               cipherAES.init(Cipher.DECRYPT_MODE, clientAESKey);
+          /* 
               byte[] decryptedUpdate = cipherAES.update(data, where, length);
               byte[] decryptedFinal = cipherAES.doFinal();
               if (decryptedFinal == null) {
@@ -159,24 +179,50 @@ public class Crypter {
               decryptedData = new byte[decryptedUpdate.length + decryptedFinal.length];
               System.arraycopy(decryptedUpdate, 0, decryptedData, 0, decryptedUpdate.length);
               System.arraycopy(decryptedFinal, 0, decryptedData, decryptedUpdate.length, decryptedFinal.length);
+          */
+              return cipherAES.doFinal(data);
           } catch (Exception e) {
               e.printStackTrace();
           }
 
           return decryptedData;
       }
+
     public static void main(String[] args) throws IOException {
         Crypter clientCrypter = new Crypter();
         clientCrypter.generateClientKeys();
         System.out.println("" +clientCrypter.getClientPublicKey() );
-        System.out.println("" +clientCrypter.getClientPrivateKey() );
-        clientCrypter.setServerPublicKey( Files.readAllBytes(Paths.get("d:\\Technical_doc\\prj\\java\\test_server\\bin\\server_pub.bin", args)));
+        System.out.println("" +clientCrypter.getClientPrivateKey().toString() );
+
         
-        byte[] Data = {1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8};
-        byte[] encrypted = clientCrypter.encryptDataAtClient(Data, 0, 16);
-        System.out.println("" +encrypted );
-        byte[] decrypted = clientCrypter.decryptDataAtClient(encrypted, 0, 16); 
-        System.out.println("" +decrypted );
+        byte[] bytes = clientCrypter.getClientPrivateKey().getEncoded();
+        //String hex = DatatypeConverter.printHexBinary(bytes);
+        byte[] bytes2 = new byte[64]; 
+        System.arraycopy(bytes, bytes.length - bytes2.length, bytes2, 0, bytes2.length);
+        
+        String hex = DatatypeConverter.printHexBinary(bytes2);
+        System.out.println(hex); // prints "7F0F00"
+        
+        //clientCrypter.setServerPublicKey( Files.readAllBytes(Paths.get("d:\\Technical_doc\\prj\\java\\test_server\\bin\\server_pub.bin", args)));
+        clientCrypter.setServerPublicKey(Files.readAllBytes(Paths.get("d:\\Technical_doc\\prj\\Lottery\\Server\\server\\HexDecode.bin", args)));
+        //String data_from_file = readFileToString(Paths.get("d:\\Technical_doc\\prj\\java\\test_server\\server_pub.txt", args));
+        //byte[] encoded = Files.readAllBytes(Paths.get("d:\\Technical_doc\\prj\\java\\test_server\\server_pub.bin", args));
+        //String data_from_file = readFile("d:\\Technical_doc\\prj\\java\\test_server\\server_pub.txt", StandardCharsets.US_ASCII);
+        //System.out.println("" +clientCrypter.getServerPublicKey());
+        //String content = new String ( Files.readAllBytes( Paths.get("d:\\Technical_doc\\prj\\java\\test_server\\server_pub.txt", args)));
+        
+
+        
+        byte[] Data = {1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7};
+        byte[] encrypted = clientCrypter.encryptDataAtClient(Data, 0, 32);
+        hex = DatatypeConverter.printHexBinary(encrypted);
+        System.out.println("encrypt" );
+        System.out.println(hex );        
+        byte[] decrypted = clientCrypter.decryptDataAtClient(encrypted, 0, 32+16); 
+        //System.out.println("" +decrypted );
+        hex = DatatypeConverter.printHexBinary(decrypted);
+        System.out.println("decrypt" );
+        System.out.println(hex); // prints "7F0F00"
         
         
         //decryptDataAtClient
